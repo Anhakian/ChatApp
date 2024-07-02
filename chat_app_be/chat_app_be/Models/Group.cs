1de82753f3b9dc5bf863c6d00c8f5c33312d40d4
Anhakian <anhnguyenngochoang2003@gmail.com>
2024-07-02 17:47:07 +0700

    user and group models & sql connection

---
+++ chat_app_be/chat_app_be/Models/Group.cs
@@ -0,0 +1,9 @@
+﻿namespace chat_app_be.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string GroupName { get; set; } = string.Empty;
+        public string GroupDescription { get; set; } = string.Empty;
+    }
+}
---
+++ chat_app_be/chat_app_be/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace chat_app_be.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Username { get; set; } = string.Empty;
+        public string DisplayName {  get; set; } = string.Empty;
+        [Required]
+        [MinLength(8)]
+        public string Password {  get; set; } = string.Empty;
+    }
+}
